8ca4366b97f5aa7f8270ef346b35b522d120680f
look4suman <look4suman@gmail.com>
2018-12-11 06:17:49 +0530

    fixed test cases

--- PerformanceTest.bat
+++ PerformanceTest.bat
@@ -1,3 +1,3 @@
-cd C:\Work\FSE\TaskManager\Api\TaskManagerService\packages\NBench.Runner.1.2.2\tools\net452
-NBench.Runner.exe "C:\Work\FSE\TaskManager\Api\TaskManagerService\TaskManager.Tests\bin\Debug\TaskManager.Tests.dll" output-directory="C:\Perf"
+cd C:\Users\Admin\Desktop\Assignments\TaskManager\Api\TaskManagerService\packages\NBench.Runner.1.2.2\tools\net452
+NBench.Runner.exe "C:\Users\Admin\Desktop\Assignments\TaskManager\Api\TaskManagerService\TaskManager.Tests\bin\Debug\TaskManager.Tests.dll" > C:\Users\Admin\Desktop\Assignments\TaskManager\Api\TaskManagerService\TestResults\PerfTests.txt
 pause
